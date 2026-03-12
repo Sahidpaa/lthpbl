de0b87d6741aa339d7274d0de6efc441d0c59183
sahidpaa <215102376+Sahidpaa@users.noreply.github.com>
2026-03-12 12:30:38 +0700

    ubah kode html

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Latihan PBL</title>
+</head>
+<body>
+    <h1>Halo, ini tugas Zulfa Sahida</h1>
+    <p>Sedang belajar Git dan Konstruksi Perangkat Lunak.</p>
+</body>
+</html>
